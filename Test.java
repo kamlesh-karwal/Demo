555d2d7c56f08f303fe3bbfb2cb5daedcb898df2
kamlesh <kamleshkarwal4@gmail.com>
2025-09-15 16:30:02 +0530

    Set

---
+++ Test.java
@@ -0,0 +1,13 @@
+package com.velocity;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		int a =10;
+		int b =10;
+		 int sum= a+b;
+		 int mul =a*b
+;		 System.out.println(sum+"and"+mul);
+	}
+}
---
+++ TestCheck.java
@@ -0,0 +1,9 @@
+package com.velocity;
+
+public class TestCheck {
+
+	public static void main(String[] args) {
+		
+		System.out.println("HEllo");
+	}
+}
